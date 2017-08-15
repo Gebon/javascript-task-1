ca0b90647cb4de74662b75d73d2adefbe914b08b
Gallyam Biktashev <gallyamb@gmail.com>
2017-08-15 19:45:02 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -1,19 +1,37 @@
 "use strict";
 
+const TO_ROMAN_NOTATION = {
+    "0": "N",
+    "1": "I",
+    "4": "IV",
+    "5": "V",
+    "9": "IX",
+    "10": "X",
+    "40": "XL",
+    "50": "L",
+    "90": "XC",
+    "100": "C",
+    "400": "CD",
+    "500": "D",
+    "900": "CM",
+    "1000": "M"
+};
+
+const ATOMIC_ROMAN_NUMBER_EQUIVALENTS = Object.keys(TO_ROMAN_NOTATION);
+
 function isInRange(value, start, end) {
     return value >= start && value < end;
 }
 
-function toRoman(value, dict) {
+function toRoman(value) {
     var result = "";
     while (value >= 0) {
-        var keys = Object.keys(dict);
-        var i = keys.length - 1;
-        while (i >= 0 && keys[i] > value) {
+        let i = ATOMIC_ROMAN_NUMBER_EQUIVALENTS.length - 1;
+        while (i >= 0 && ATOMIC_ROMAN_NUMBER_EQUIVALENTS[i] > value) {
             i--;
         }
-        value -= keys[i];
-        result += dict[keys[i]];
+        value -= ATOMIC_ROMAN_NUMBER_EQUIVALENTS[i];
+        result += TO_ROMAN_NOTATION[ATOMIC_ROMAN_NUMBER_EQUIVALENTS[i]];
         if (value === 0) {
             break;
         }
@@ -23,11 +41,11 @@ function toRoman(value, dict) {
 }
 
 function isCorrectTime(parts) {
-    return parts.length !== 2 ||
-        parts[0].length !== 2 ||
-        parts[1].length !== 2 ||
-        !isInRange(parts[0], 0, 24) ||
-        !isInRange(parts[1], 0, 60);
+    return parts.length === 2 &&
+        parts[0].length === 2 &&
+        parts[1].length === 2 &&
+        isInRange(parts[0], 0, 24) &&
+        isInRange(parts[1], 0, 60);
 }
 
 /**
@@ -36,31 +54,14 @@ function isCorrectTime(parts) {
  */
 function romanTime(time) {
     if (time === undefined || time === null) {
-        throw new TypeError();
+        throw new TypeError("time argument is null or undefined);
     }
     var parts = time.split(":");
-    if (isCorrectTime(parts)) {
-        throw new TypeError();
+    if (!isCorrectTime(parts)) {
+        throw new TypeError("Argument is not a correct time representation. Expected time in following format: HH:MM");
     }
-    var dict = {
-        "0": "N",
-        "1": "I",
-        "4": "IV",
-        "5": "V",
-        "9": "IX",
-        "10": "X",
-        "40": "XL",
-        "50": "L"
-    };
-    var result = "";
-    for (var i = 0; i < parts.length; i++) {
-        if (result !== "") {
-            result += ":";
-        }
-        result += toRoman(parseInt(parts[i]), dict);
-    }
-
-    return result;
+    
+    return parts.map(part => toRoman(parseInt(part))).join(":");
 }
 
 module.exports = romanTime;
